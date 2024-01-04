d94ef235b78b10bc637b5c5bb9b401ffd3d4300b
David Petrofsky <davidpet@ucla.edu>
2024-01-03 17:37:53 -0800

    Adding a post-debate summary phase.

--- machine_learning/safron/safron.py
+++ machine_learning/safron/safron.py
@@ -5,9 +5,7 @@ import sys
 from termcolor import colored
 
 from machine_learning.common import utilities
-from machine_learning.common.openai_api import fetch_api_key, Chat
-
-TEMPERATURE = 1.0
+from machine_learning.common.openai_api import fetch_api_key, Chat, prompt
 
 
 def debate(chat1: Chat, chat2: Chat, rounds: int) -> None:
@@ -18,7 +16,7 @@ def debate(chat1: Chat, chat2: Chat, rounds: int) -> None:
         chat1.add_user_msg(neg_msg)
 
 
-def print_chat(chat: Chat) -> None:
+def print_chat(chat: Chat, colorize=False) -> None:
     print()
 
     for message in chat.messages:
@@ -31,8 +29,16 @@ def print_chat(chat: Chat) -> None:
         else:
             continue
 
-        print(f'{colored(label, color)}:', message['content'])
+        if colorize:
+            print(f'{colored(label, color)}:', message['content'])
+        else:
+            print(f'{label}: {message["content"]}')
         print()
+
+
+def summarize(chat: Chat, system: str):
+    summary = prompt(str(chat), system=system)
+    print(f'{colored("summary", "blue")}:\n{summary}')
 
 
 def main() -> int:
@@ -40,23 +46,25 @@ def main() -> int:
         files={
             'affirmative': 'messages/system_affirmative.txt',
             'negative': 'messages/system_negative.txt',
+            'summary': 'messages/system_summary.txt',
         })
 
     if not fetch_api_key():
         print('ERROR: API key not found!')
         return 1
 
-    prompt = input('Please enter debate topic: ').strip()
+    topic = input('Please enter debate topic: ').strip()
 
     affirmative_chat = Chat()
-    affirmative_chat.add_system_msg(messages['affirmative'].format(prompt))
+    affirmative_chat.add_system_msg(messages['affirmative'].format(topic))
     negative_chat = Chat()
-    negative_chat.add_system_msg(messages['negative'].format(prompt))
+    negative_chat.add_system_msg(messages['negative'].format(topic))
 
     rounds = int(input("Enter number of rounds: ").strip())
 
     debate(affirmative_chat, negative_chat, rounds)
-    print_chat(affirmative_chat)
+    print_chat(affirmative_chat, colorize=True)
+    summarize(affirmative_chat, messages['summary'])
 
     return 0
 
